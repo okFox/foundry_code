5020645dfe10cec258ce8b84fcfaf4ddfd5c3715
okFox <smayfield@gmail.com>
2021-02-20 22:31:30 -0800

    lockpick macro reqs

---
+++ lockpick.js
@@ -0,0 +1,24 @@
+// Dialogue pop up box asking for DC of lock and required number of Successes
+// Continuously rolls the selected characters thievery roll against the DC and counts the number of rolls made until one of two conditions are met. 
+// Either A) The number of Successes has been met (at which point output the total number of attempts made times 6 / or if it's easier just total number of attempts made)
+// Or B) a critical failure occurs (so DC-10) at which point again output total number of attempts made either times 6 or not
+
+
+
+// get DC
+
+// get number of successes needed
+
+//set crit fail value (DC - 10)
+
+//track attempts and keep rolling until:
+
+    // === num of successes rolled
+        //  return attempts * 6
+    
+// OR
+
+    //crit fail rolled
+        // return attempts * 6
+
+
