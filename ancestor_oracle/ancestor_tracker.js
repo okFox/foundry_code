dfbc1dbb7108dc37e53f368e55283c5fadf5dbeb
okFox <smayfield@gmail.com>
2021-03-19 15:59:53 -0700

    adding easy functions based on known interface elements

--- ancestor_oracle/ancestor_tracker.js
+++ ancestor_oracle/ancestor_tracker.js
@@ -16,15 +16,36 @@
 // Skill - +2 to perception and skill checks
 // Cast - non-cantrip spells w/o a duration gain dmg/heal bonus equal to spell's level + 3
 
+
+//TODO
+// save state: to journal?  to gameDB?
+//----------------------------------------------------------------------------------------------------
+
 let dailyAncestor = 0; // rolled at beginning of each day; this is default when not in encounter
 let encounterAncestor = 0; // determined at end of each turn during encounter
 let curseLevel = 0; //increments when focus points used; decrements when 10min refocus
+let inEncounter = false;
 
 
 
-const anMartial = (curselvl) => {
-//set roll mod
-//set dmg mod
+const anMartial = (curseLevel) => {
+//set roll mod?
+//set dmg mod?
 //change interface
 
+}
+
+//BUTTON: revert to daily ancestor
+const endEncounter = () => {
+    return encounterAncestor = 0;
+}
+
+//DROPDOWN: select ancestor when a 4 is rolled
+const selectAncestor = (inEncounter, userSelect) => {
+    if (inEncounter === true) {
+        encounterAncestor = userSelect
+    } else {
+        dailyAncestor = userSelect
+    }
+    return
 }
