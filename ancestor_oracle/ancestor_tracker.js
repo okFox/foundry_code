b539a042cecd8808f6a4e87c8d218ef3229a5097
okFox <smayfield@gmail.com>
2021-03-19 15:38:43 -0700

    adding oracle rules

---
+++ ancestor_oracle/ancestor_tracker.js
@@ -0,0 +1,30 @@
+
+//minor curse(1)
+// roll a d4 to determine day's ancestor
+// if 1 ancestor = anMartial
+// if 2 ancestor = anSkill
+// if 3 ancestor = anCast
+// if 4 select ancestor to use
+
+//moderate curse (2)):
+// 1: Martial - +1 atk rolls; +2 dmg
+// 2: Skill - +1 to perception and skill checks
+// 3: Cast - non-cantrip spells w/o a duration gain dmg/heal bonus equal to spell's level
+
+//major curse(3):
+// Martial - +1 atk rolls; +6 dmg
+// Skill - +2 to perception and skill checks
+// Cast - non-cantrip spells w/o a duration gain dmg/heal bonus equal to spell's level + 3
+
+let dailyAncestor = 0; // rolled at beginning of each day; this is default when not in encounter
+let encounterAncestor = 0; // determined at end of each turn during encounter
+let curseLevel = 0; //increments when focus points used; decrements when 10min refocus
+
+
+
+const anMartial = (curselvl) => {
+//set roll mod
+//set dmg mod
+//change interface
+
+}
